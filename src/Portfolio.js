775d0e6239efe4826f3939823b9963068e0a7322
XaviGrandes <xavigr92@gmail.com>
2023-01-13 11:29:59 +0100

    Update image profile

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,23 +1,23 @@
-function App() {
+import React from 'react';
+import xaviGrandes from './images/Xavi-Grandes.png';
+
+export default function App() {
   return (
-    <main className="bg-blue-100 h-full">
-      <section className="profile">
-      <h1 className="profile_name" >Xavi Grandes Rovira</h1>
-      <h2 className="profile_work">Full stack developer</h2>
-      <p className="profile_intro">Graduated in business management and marketing with a master's degree in digital marketing, I started the adventure working for different companies and now I decided to make a job change.
-      <br/>
-      Recently I finished my bootcamp and I'm gratuated as a Full Stack developer. I learn programming languages and frameworks (HTML, CSS, JavaSript with React and also Insomnia, Nodejs, MongoDB and Mongoose)
-      I continue studying on my own, reviewing the acquired knowledge and new programming technologies while looking for a job to become a great professional in the sector.
-      <br/>
-      Here I show you my projects</p>
-      <div>
-        <i className="profile_socialMedia_Linkedin"></i>
-        <i className="profile_socialMedia_Github"></i>
-      </div>
-      <img/>
+    <main className="bg-[#111827] h-full">
+      <section className="flex flex-col text-center gap-3 pt-10">
+        <h1 className="text-[#6fd0c8] text-4xl " >Xavi Grandes Rovira</h1>
+        <h2 className="text-white text-2xl">Full stack developer</h2>
+        <p className="text-white">Graduated in business management and marketing with a master's degree in digital marketing.
+        <br/>
+        Recently I finished my bootcamp and I'm gratuated as a Full Stack developer. I learn programming languages and frameworks (HTML, CSS, JavaSript with React and also Insomnia, Nodejs, MongoDB and Mongoose)
+        <br/>
+        Here I show you my projects</p>
+        <div>
+          <a className="text-white">Linkedin</a>
+          <a className="text-white">Github</a>
+        </div>
+        <img className='h-full w-full border rounded-[50%]' src="{xaviGrandes}" alt='image profile'/>
       </section>
     </main>
   );
 }
-
-export default App;
