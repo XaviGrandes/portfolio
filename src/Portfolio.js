859700f883753ebfd3bcb0c03ef24621f8e672a8
XaviGrandes <xavigr92@gmail.com>
2023-01-13 10:52:18 +0100

    Rename App to Profolio

---
+++ src/Portfolio.js
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <main className="bg-blue-100 h-full">
+      <section className="profile">
+      <h1 className="profile_name" >Xavi Grandes Rovira</h1>
+      <h2 className="profile_work">Full stack developer</h2>
+      <p className="profile_intro">Graduated in business management and marketing with a master's degree in digital marketing, I started the adventure working for different companies and now I decided to make a job change.
+      <br/>
+      Recently I finished my bootcamp and I'm gratuated as a Full Stack developer. I learn programming languages and frameworks (HTML, CSS, JavaSript with React and also Insomnia, Nodejs, MongoDB and Mongoose)
+      I continue studying on my own, reviewing the acquired knowledge and new programming technologies while looking for a job to become a great professional in the sector.
+      <br/>
+      Here I show you my projects</p>
+      <div>
+        <i className="profile_socialMedia_Linkedin"></i>
+        <i className="profile_socialMedia_Github"></i>
+      </div>
+      <img/>
+      </section>
+    </main>
+  );
+}
+
+export default App;
